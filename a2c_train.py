a02032e242e38f6a5512ee4b7661487d8e184ffe
michealgrab <grabmi.mg@gmail.com>
2025-08-19 13:05:26 +0200

    implement the training algorithm

--- a2c_train.py
+++ a2c_train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import gymnasium as gym
 import torch.optim as optim
+from torch.distributions import Categorical
 
 class Actor(nn.Module):
     def __init__(self, input_size, num_actions):
@@ -50,7 +51,11 @@ def train(
         done = False
         ep_return = 0
         action_tensor = actor(obs_tensor)
-        action = torch.argmax(action_tensor, dim=-1).item()
+
+        # sample the action
+        dist = Categorical(action_tensor)
+        action = dist.sample().item()
+
         print("episode starts")
         while not done:
             next_obs, rew, terminated, truncated, info = env.step(action)
@@ -58,14 +63,54 @@ def train(
             step_count = step_count + 1
             next_obs_tensor = torch.FloatTensor(next_obs).unsqueeze(0)
             action_tensor = actor(next_obs_tensor)
-            action = torch.argmax(action_tensor, dim=-1).item()
-            print(f"step: {step_count}, action: {action}, reward {rew}")
+
+            # sample the action
+            dist = Categorical(action_tensor)
+            action_sample = dist.sample()
+            action = action_sample.item()
+
+            value_tensor = critic(obs_tensor)
+            next_value_tensor = critic(next_obs_tensor) 
+
+            # calculate the TD-target
+            # TD target is meant to be a fixed reference point for the loss  
+            # should not be used for optimizer adjust
+            # so we use .detach() 
+            td_target = rew + gamma * next_value_tensor.detach() * (1 - done)
+
+            # calcualte the advantage function
+            advantage = td_target - value_tensor.detach()
+
+            # calculate the log_prob
+            # pass the index of the chosen action in form of a tensor
+            log_prob = dist.log_prob(action_sample)
+
+            # calculate the actor loss
+            actor_loss = -1 * log_prob * advantage
+
+            # calculate the critic loss
+            critic_loss = F.mse_loss(td_target, value_tensor)
+
+            # optimize the actor
+            optim_actor.zero_grad()
+            actor_loss.backward()
+            optim_actor.step()
+
+            # optimize the critic
+            optim_critic.zero_grad()
+            critic_loss.backward()
+            optim_critic.step()
+
+            print(f"step: {step_count}, action: {action}, reward {rew}, actor loss: {actor_loss.item():.2f}, critic loss: {critic_loss.item():.2f}")
+
+            ep_return += rew
             obs = next_obs
+            obs_tensor = next_obs_tensor
 
 env = gym.make("CartPole-v1")
 input_size = env.observation_space.shape[0]
 num_actions = env.action_space.n
 actor = Actor(input_size, num_actions)
-critic = Critic(num_actions)
+critic = Critic(input_size)
 
 train(env, actor, critic, nb_steps=100)
--- forward_pass_test.py
+++ forward_pass_test.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+gamma = 0.99
+
 class Actor(nn.Module):
     def __init__(self, input_size, num_actions):
         super().__init__()
@@ -26,6 +28,7 @@ class Critic(nn.Module):
         return x
     
 import gymnasium as gym
+from  torch.distributions import Categorical
 
 env = gym.make('CartPole-v1')
 input_size = env.observation_space.shape[0]
@@ -40,28 +43,45 @@ obs, info = env.reset()
 print(f"obs.dtype: {obs.dtype}")
 print(f"type(obs): {type(obs)}")
 
-print("torch.FloatTensor(obs)")
-obs = torch.FloatTensor(obs)
+obs_tensor = torch.FloatTensor(obs)
+obs_tensor = obs_tensor.unsqueeze(0)
+done = False
+ep_return = 0
+action_tensor = actor(obs_tensor)
+print("softmax action tensor: ")
+print(action_tensor)
 
-print(f"obs.dtype: {obs.dtype}")
-print(f"type(obs): {type(obs)}")
+# sample the action
+dist = Categorical(action_tensor)
+action = dist.sample().item()
 
-action = actor(obs)
+print(f"type(dist.sample()): {type(action)}")
 
-print(f"type(action): {type(action)}")
+next_obs, rew, terminated, truncated, info = env.step(action)
+done = terminated or truncated
+next_obs_tensor = torch.FloatTensor(next_obs).unsqueeze(0)
+action_tensor = actor(next_obs_tensor)
 
-# Convert tensor to Python int 
-print("action = action.item()")
-# action = action.item()
+value_tensor = critic(obs_tensor)
+next_value_tensor = critic(next_obs_tensor)  
 
-print("softmax tensor: ")
-print(action)
-# print(f"type(action): {type(action)}")
+print(f"next_value tensor: {next_value_tensor}")
+print(f"next_value tensor.detach(): {next_value_tensor.detach()}")
 
-action = torch.argmax(action, dim=-1)
-action = action.item()
-print(f"action {action}")
-print(f"type(action): {type(action)}")
+# calculate the TD-target
+td_target = rew + gamma * next_value_tensor * (1 - done)
+print(f"TD-target: {td_target}")
 
+# sample the action
+dist = Categorical(action_tensor)
+action = dist.sample()
+
+# log_prob
+log_prob = dist.log_prob(action)
+print(f"log_prob: {log_prob}")
+
+
+obs = next_obs
+obs_tensor = next_obs_tensor
 # env.step(action)
 
